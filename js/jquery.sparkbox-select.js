c8573f5047432ffc419d9485ea6c4a8c474d2f2c
mixer2 <mixer2@gmail.com>
2012-02-11 00:44:48 +0100

    * get rid of the data-id stuff... replaced with references to jquery objects (improved performance and correct behavior for selects with 2 options with the same value) * removed selectboxCounter * performance improvements * cleaned source * event delegation for all events (cleaner source + better performance) * bugfix for scroll position recovery * updated tests to match fixed behavior

--- js/jquery.sparkbox-select.js
+++ js/jquery.sparkbox-select.js
@@ -1,6 +1,5 @@
 (function($) {
-  var selectboxCounter = 0;
-  
+
   $.fn.sbCustomSelect = function(options) {
     var iOS = (navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)),
         android = (navigator.userAgent.match(/Android/i)),
@@ -10,91 +9,89 @@
           appendTo: false
         }, options);
 
-    var scrollToSelected = function() {
-        var $selected = $('.sb-dropdown:visible li.selected');
+    var scrollToSelected = function($dropdown, recover) {
+        var $selected = $dropdown.find('li.selected');
         if(!$selected.length) return true;
 
         //recover scroll position (after fadeOut)
-        if($selected.parent().data('sb-saved-scrollTo')) $selected.parent().scrollTop($selected.parent().data('sb-saved-scrollTo'));
+        if($dropdown.data('sb-saved-scrollTo') && recover) $dropdown.scrollTop($dropdown.data('sb-saved-scrollTo'));
 
         //we can't just use $selected.position().top, if the sb-dropdown is positioned static -.-
-        var scrollTop = $selected.parent().scrollTop(),
-        offset = ($selected.offset().top+scrollTop-$selected.parent().offset().top);
+        var scrollTop = $dropdown.scrollTop(),
+        offset = ($selected.offset().top+scrollTop-$dropdown.offset().top);
 
         if(offset < scrollTop) {
-            $selected.parent().scrollTop(offset);
-            $selected.parent().data('sb-saved-scrollTo', offset)
-        } else if($selected.parent().innerHeight()+scrollTop < offset+$selected.outerHeight()) {
-            var scrollTo = offset+$selected.outerHeight()-$selected.parent().innerHeight();
-            $selected.parent().scrollTop(scrollTo);
-            $selected.parent().data('sb-saved-scrollTo', scrollTo);
+            $dropdown.scrollTop(offset);
+        } else if($dropdown.innerHeight()+scrollTop < offset+$selected.outerHeight()) {
+            var scrollTo = offset+$selected.outerHeight()-$dropdown.innerHeight();
+            $dropdown.scrollTop(scrollTo);
         }
+        $dropdown.data('sb-saved-scrollTo', $dropdown.scrollTop());
     }
 
     // Sync custom display with original select box and set selected class and the correct <li>
-    var updateSelect = function() {
-      var $this = $(this),
-          $dropdown = $('.sb-dropdown[data-id=' + $this.parent().data('id') + ']'),
-          $sbSelect = $this.siblings('.sb-select');
+    var updateSelect = function(select) {
+      var $select = this === window ? $(select) : $(this),
+          $dropdown = $select.parent().data('sb-dropdown'),
+          $sbSelect = $select.siblings('.sb-select');
 
-      if (this.selectedIndex != -1) {
-        $sbSelect.val(this[this.selectedIndex].innerHTML);
-      
-        var $selected = $dropdown.children().removeClass('selected').eq(this.selectedIndex).addClass('selected');
+      if ($select.attr("selectedIndex") != -1) {
+        $sbSelect.val($select.children().eq($select.attr("selectedIndex")).html());
 
-        scrollToSelected();
+        if($dropdown) {
+            $dropdown.children('.selected').removeClass('selected');
+            $dropdown.children().eq($select.attr("selectedIndex")).addClass('selected');
+            scrollToSelected($dropdown);
+        }
       }
     };
 
     // Update original select box, hide <ul>, and fire change event to keep everything in sync
     var dropdownSelection = function(e, preventClose) {
       e.preventDefault();
-      var $target = $(this).children('li').andSelf().filter($(e.target).parents('li').andSelf()).filter('li');
+      var $target = $(e.target).parent('li').andSelf().filter('li');
       if(!$target.length) return true;
 
-      var id = $target.parent().attr('data-id'),
-          $option = $('.sb-custom[data-id=' + id + ']').find('option').filter('[value="' + $target.data('value') + '"]');
+      var $option = $target.data('sb-option');
 
       $option[0].selected = true;
       if(!preventClose) {
           hideDropdown({});
           if($option.parent().data('lastVal') != $option.parent().val()) $option.parent().data('lastVal', $option.parent().val()).trigger('change');
-      } else $option.trigger('sb-sync');
+      } else updateSelect($option.parent());
     };
     
     // Create the <ul> that will be used to change the selection on a non iOS/Android browser
-    var createDropdown = function($select, i) {
+    var createDropdown = function($select) {
       var $options = $select.children(),
-          $dropdown = $('<ul data-id="' + i + '" class="sb-dropdown"/>');
+          $dropdown = $('<ul class="sb-dropdown"/>').data('sb-custom', $select.parent());
       
       $options.each(function() {
         $this = $(this);
-        $dropdown.append('<li data-value="' + $this.val() + '"><a href=".">' + $this.text() + '</a></li>');
+        $('<li><a href=".">' + $this.text() + '</a></li>').data('sb-option', $this).appendTo($dropdown);
       });
-      $dropdown.bind('click', dropdownSelection);
       
       return $dropdown;
     };
     
     // Clear keystroke matching string and show dropdown
     var viewList = function(e) {
-      var $this = $(this),
-          id = $this.data('id');
-          
+      var $dropdown = $(this).parent().data('sb-dropdown') || $(this);
+
       clearKeyStrokes();
 
-      $('.sb-dropdown[data-id!=' + id + ']').fadeOut('fast');
-      $('.sb-dropdown[data-id=' + id + ']').fadeIn('fast');
+      hideDropdown({target: $dropdown});
+      $dropdown.fadeIn('fast');
 
-      scrollToSelected();
+      scrollToSelected($dropdown, true);
 
       e.preventDefault();
     };
     
     // Hide the custom dropdown
     var hideDropdown = function(e) {
-        var id = $(e.target).closest('.sb-dropdown').add($(e.target).closest('.sb-custom')).data('id'),
-            $prevent = $('.sb-dropdown[data-id=' + id + ']');
+        var $matches = $(e.target).closest('.sb-dropdown, .sb-custom'),
+            $prevent = $matches.data('sb-dropdown') || $matches;
 
         $('.sb-dropdown').not($prevent).fadeOut('fast');
     };
@@ -102,7 +99,7 @@
     // Manage keypress to replicate browser functionality
     var selectKeypress = function(e) {
       var $this = $(this),
-          $current = $('.sb-dropdown[data-id=' + $this.data('id') + ']').find('.selected');
+          $current = $this.parent().data('sb-dropdown').find('.selected');
 
       //select in dropdown if it's open... else directly change select value to target element
       function softSelect(el) {
@@ -206,12 +203,13 @@
      * After all of the setup is complete, trigger the change event on the original select box to update the .sb-select input
      */
     this.each(function() {
-      var $self = $(this);
+      var $self = $(this),
+          $sbCustom = $('<div class="sb-custom"/>');
 
       $self.attr('tabindex', -1)
-        .wrap('<div data-id="' + selectboxCounter + '" class="sb-custom"/>')
-        .after('<input data-id="' + selectboxCounter + '" type="text" class="sb-select" readonly="readonly" />')
-        .bind('change sb-sync', updateSelect);
+        .after($sbCustom)
+        .appendTo($sbCustom)
+        .after('<input type="text" class="sb-select" readonly="readonly" />');
       
       
       if (iOS || android) {
@@ -224,34 +222,36 @@
           'z-index': 1000
         });
       } else {
-        
-        $self.next().bind('click', viewList);
-        
+
+        $sbCustom.data('sb-dropdown', createDropdown($self));
+
         if (!settings.appendTo) {
-          $self.after(createDropdown($self, selectboxCounter));
+            $sbCustom.append($sbCustom.data('sb-dropdown'));
         } else {
           var offset = $self.parent().offset();
-          
-          $(settings.appendTo).append(createDropdown($self, selectboxCounter).css({
+
+          $(settings.appendTo).append($sbCustom.data('sb-dropdown').css({
             'top': offset.top,
             'left': offset.left,
             'width': $self.parent().width() * 0.8
           }));
         }
+
+        $self.data('lastVal', $self.val());
       }
 
-      $self.data('lastVal', $self.val());
-      $self.trigger('sb-sync');
-      selectboxCounter++;
+      updateSelect($self);
     });
     
     // Hide dropdown when click is outside of the input or dropdown
-    $(document).bind('click', hideDropdown);
-    
-    $('.sb-custom').find('.sb-select').live('keydown', selectKeypress);
-    $('.sb-custom').bind('blur', clearKeyStrokes);
-    $(document).delegate('.sb-dropdown', 'focus', viewList);
-    
+    $(document).bind('click', hideDropdown)
+        .delegate('.sb-custom', 'blur', clearKeyStrokes)
+        .delegate('.sb-custom select', 'change', updateSelect)
+        .delegate('.sb-select', 'keydown', selectKeypress)
+        .delegate('.sb-select', 'click', viewList)
+        .delegate('.sb-dropdown', 'focus', viewList)
+        .delegate('.sb-dropdown', 'click', dropdownSelection);
+
     return this;
   };
 })(jQuery);
